204d5115671f0595ea6656c2c6f4581e6b05f0c4
Marina-tarek <marina.tarek.w@gmail.com>
2025-01-29 23:57:17 +0200

    Q

--- Assignment Advance02/Program.cs
+++ Assignment Advance02/Program.cs
@@ -42,6 +42,57 @@ namespace Assignment_Advance02
             #endregion
 
             #region Given a number N and an array of N numbers. Determine if it's palindrome or not.
+            // Ex:
+            //  Input:
+            //   5
+            //1 3 2 3 1
+            //Output:
+            //   YES
+
+
+            #endregion
+
+            #region Given a Queue, implement a function to reverse the elements of a queue using a stack.
+
+            #endregion
+
+
+            #region 4. Given a Stack, implement a function to check if a string of parentheses is balanced using a stack.
+            //Ex:
+            //Input:
+            //    [()] { }
+            //Output:
+            //    Balanced
+
+
+            #endregion
+
+            #region 5. Given an array, implement a function to remove duplicate elements from an array.
+
+            #endregion
+
+            #region 6. Given an array list , implement a function to remove all odd numbers from it.
+
+            #endregion
+
+            #region 7- Implement a queue that can hold different data types. 
+
+            #endregion
+
+
+            #region 8- Create a function that pushes a series of integers onto a stack. Then, search for a target integer in the stack. If the target is found, print a message indicating that the target was found how many elements were checked before finding the target (“Target was found successfully and the count = 5”). If the target is not found, print a message indicating that the target was not found(“Target was not found”).
+
+            #endregion
+
+            #region 9. Given two arrays, find their intersection. Each element in the result should appear as many times as it shows in both arrays..
+
+            #endregion
+
+            #region 10. Given an ArrayList of integers and a target sum, find if there is a contiguous sub list that sums up to the target.
+
+            #endregion
+
+            #region 11. Given a queue reverse first K elements of a queue, keeping the remaining elements in the same order 
 
             #endregion
         }
